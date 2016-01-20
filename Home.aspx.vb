1ef48ffdd8426ddb8fb1a0927d3b811eb14f48e2
CypherDragoon88 <abdul_naqshband@live.co.uk>
2016-01-20 00:21:31 +0000

    Create Home.aspx.vb

---
+++ Home.aspx.vb
@@ -0,0 +1,9 @@
+@@ -0,0 +1,8 @@
+﻿
+Partial Class Home
+    Inherits System.Web.UI.Page
+
+    Protected Sub ImageMap1_Click(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.ImageMapEventArgs) Handles ImageMap1.Click
+
+    End Sub
+End Class
