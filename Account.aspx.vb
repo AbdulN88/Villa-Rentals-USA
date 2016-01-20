d225efbd114a2297c662037b99db480b3477f42b
CypherDragoon88 <abdul_naqshband@live.co.uk>
2016-01-20 00:09:49 +0000

    Create Account.aspx.vb

---
+++ Account.aspx.vb
@@ -0,0 +1,9 @@
+@@ -0,0 +1,8 @@
+﻿
+Partial Class Home
+    Inherits System.Web.UI.Page
+
+    Protected Sub Login1_Authenticate(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.AuthenticateEventArgs)
+
+    End Sub
+End Class
