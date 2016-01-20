a990332fb2d1dcb8d8edbdff1ef12b9d6e03a4a8
CypherDragoon88 <abdul_naqshband@live.co.uk>
2016-01-20 00:18:36 +0000

    Create Customers_Details.aspx.vb

---
+++ Customers_Details.aspx.vb
@@ -0,0 +1,8 @@
+@@ -0,0 +1,7 @@
+﻿
+Partial Class Home
+    Inherits System.Web.UI.Page
+
+    Protected Sub ButtonSearch_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles ButtonSearch.Click
+    End Sub
+End Class
